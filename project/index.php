f4bb745b9ff208dc3a80f9f80feefc74054d25ce
raffienanda <raffienanda@gmail.com>
2025-12-03 23:35:12 +0700

    tes

--- project/index.php
+++ project/index.php
@@ -1,31 +1,118 @@
 <?php
-// Main Router
-$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+// --- LOAD SEMUA VIEWMODELS ---
+include_once 'viewmodels/MemberViewModel.php';
+include_once 'viewmodels/TrainerViewModel.php';
+include_once 'viewmodels/EquipmentViewModel.php';
+include_once 'viewmodels/SessionViewModel.php';
 
+// --- ROUTING ---
+$page = isset($_GET['page']) ? $_GET['page'] : 'members';
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+// --- HEADER ---
+include 'views/template/header.php';
+// Tambahin link navigasi baru di header.php kamu secara manual ya:
+// <a href="index.php?page=equipment">Alat Gym</a>
+
+// --- LOGIC SWITCH ---
 switch ($page) {
-    case 'teams':
-        require_once 'viewmodels/TeamViewModel.php';
-        $vm = new TeamViewModel();
-        $vm->handleRequest();
+    // 1. ANGGOTA
+    case 'members':
+        $viewModel = new MemberViewModel();
+        if ($action == 'create') {
+            include 'views/member_form.php';
+        } elseif ($action == 'edit') {
+            $member = $viewModel->fetchById($_GET['id']);
+            include 'views/member_form.php';
+        } elseif ($action == 'store') {
+            if (!empty($_POST['id'])) {
+                $viewModel->update($_POST);
+            } else {
+                $viewModel->create($_POST);
+            }
+            header("Location: index.php?page=members");
+        } elseif ($action == 'delete') {
+            $viewModel->delete($_GET['id']);
+            header("Location: index.php?page=members");
+        } else {
+            $members = $viewModel->fetchAll();
+            include 'views/member_list.php';
+        }
         break;
-        
-    case 'players':
-        // require_once 'viewmodels/PlayerViewModel.php';
-        // $vm = new PlayerViewModel();
-        // $vm->handleRequest();
-        echo "Silakan copy struktur TeamViewModel untuk Players!";
+
+    // 2. PELATIH
+    case 'trainers':
+        $viewModel = new TrainerViewModel();
+        if ($action == 'create') {
+            include 'views/trainer_form.php';
+        } elseif ($action == 'edit') {
+            $trainer = $viewModel->fetchById($_GET['id']);
+            include 'views/trainer_form.php';
+        } elseif ($action == 'store') {
+            if (!empty($_POST['id'])) {
+                $viewModel->update($_POST);
+            } else {
+                $viewModel->create($_POST);
+            }
+            header("Location: index.php?page=trainers");
+        } elseif ($action == 'delete') {
+            $viewModel->delete($_GET['id']);
+            header("Location: index.php?page=trainers");
+        } else {
+            $trainers = $viewModel->fetchAll();
+            include 'views/trainer_list.php';
+        }
         break;
+
+    // 3. EQUIPMENT
+    case 'equipment':
+        $viewModel = new EquipmentViewModel();
+        if ($action == 'create') {
+            include 'views/equipment_form.php';
+        } elseif ($action == 'edit') {
+            $equipment = $viewModel->fetchById($_GET['id']);
+            include 'views/equipment_form.php';
+        } elseif ($action == 'store') {
+            if (!empty($_POST['id'])) {
+                $viewModel->update($_POST);
+            } else {
+                $viewModel->create($_POST);
+            }
+            header("Location: index.php?page=equipment");
+        } elseif ($action == 'delete') {
+            $viewModel->delete($_GET['id']);
+            header("Location: index.php?page=equipment");
+        } else {
+            $equipmentList = $viewModel->fetchAll();
+            include 'views/equipment_list.php';
+        }
+        break;
+
+    // 4. SESSIONS (JADWAL) - RELASI
+    case 'sessions':
+        $sessionViewModel = new SessionViewModel();
         
-    // Tambahkan case untuk 'categories' dan 'staff' disini
-        
+        if ($action == 'create') {
+            // Kita butuh data Member dan Trainer untuk Dropdown
+            $memberVM = new MemberViewModel();
+            $trainerVM = new TrainerViewModel();
+            
+            $membersList = $memberVM->fetchAll();
+            $trainersList = $trainerVM->fetchAll();
+            
+            include 'views/session_form.php';
+        } elseif ($action == 'store') {
+            $sessionViewModel->create($_POST);
+            header("Location: index.php?page=sessions");
+        } else {
+            $sessions = $sessionViewModel->fetchAll();
+            include 'views/session_list.php';
+        }
+        break;
+
     default:
-        echo "<h1>Dashboard Olahraga</h1>";
-        echo "<ul>
-                <li><a href='index.php?page=categories'>Manage Categories</a></li>
-                <li><a href='index.php?page=teams'>Manage Teams</a></li>
-                <li><a href='index.php?page=players'>Manage Players</a></li>
-                <li><a href='index.php?page=staff'>Manage Staff</a></li>
-              </ul>";
-        break;
+        echo "<h2>404 - Halaman tidak ditemukan</h2>";
 }
-?>
+
+echo "</body></html>";
+?>                      
